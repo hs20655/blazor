7132905a533725375f4a886764105f53fbc8da92
Taras Kuzyk <kuzyk.taras1989@gmail.com>
2022-11-16 19:56:16 +0200

    MS identity init

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,68 @@
 using Data;
+using Data.Entities;
 using Logic.Core.UnitOfWork.Configuration;
 using Logic.Core.UnitOfWork.IConfiguration;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
+
+//builder.Services.AddControllers();
+
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//CORS
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+//
+builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ModelContext>(); 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// For Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ModelContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
+});
+
 
 builder.Services.AddControllers();
 
@@ -15,20 +70,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//CORS
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
-builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddScoped<ModelContext>(); 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-
-
 var app = builder.Build();
 
-//var con = new ModelContext(); //CREATE DB FOR TEST
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Data/ModelContext.cs
+++ Data/ModelContext.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Configurations;
 using Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,14 +10,41 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    public class ModelContext : DbContext
+    //public class ModelContext : DbContext
+    //{
+    //    public DbSet<Customer> Customers { get; set; }
+
+    //    public ModelContext()
+    //    {
+    //       // Database.EnsureDeleted();
+    //      // Database.EnsureCreated();
+    //    }
+
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        optionsBuilder.UseSqlServer(
+    //        @"Server=(localdb)\MSSQLLOcalDB;Database=BLAZOR;Trusted_Connection=True;");
+    //    }
+
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+
+    //        base.OnModelCreating(modelBuilder);
+    //        modelBuilder.ApplyConfiguration(new CustomersConfiguration());
+
+    //    }
+
+    //}
+
+    //IdentityDbContext and not DbContext (need identity tables)
+    public class ModelContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
 
         public ModelContext()
         {
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
