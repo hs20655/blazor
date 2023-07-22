ee24facc0ed893876a183705aa9b4fd85e935d99
hs20655 <kuzyk.taras1989@gmail.com>
2023-07-22 19:49:55 +0300

    small changes in names

--- Patterns/CreationalPatterns/CreationalPattern.cs
+++ Patterns/CreationalPatterns/CreationalPattern.cs
@@ -15,14 +15,18 @@ namespace Patterns.CreationalPatterns
         {
             //Singleton is a creational design pattern that lets you ensure that a class has only one instance,
             //while providing a global access point to this instance.
-            Singleton();
+            SingletonExecute();
             
             //Prototype is a creational design pattern that lets you copy existing objects
             //without making your code dependent on their classes.
-            Prototype();
+            PrototypeExecute();
+
+            //Builder is a creational design pattern that lets you construct complex objects step by step.
+            //The pattern allows you to produce different types and representations of an object using the same construction code.
+            BuilderExecute();
         }
 
-        private static void Singleton()
+        private static void SingletonExecute()
         {
             SingletonExample singletonFirst = SingletonExample.GetInstance();
             SingletonExample singletonSecond = SingletonExample.GetInstance();
@@ -39,8 +43,7 @@ namespace Patterns.CreationalPatterns
                 //ERROR
             }
         }
-
-        private static void Prototype()
+        private static void PrototypeExecute()
         {
             //initiate prototype person1 .Create person2 and person3 same like person1
             PrototypeExample prototypeExample = new PrototypeExample(34, Convert.ToDateTime("1989-04-01"), "Taras", 12345);
@@ -50,6 +53,10 @@ namespace Patterns.CreationalPatterns
             prototypeExample.ModifyPrototype(null, null, null, null);
             //get cloned object after modifying original prototype (check results)
             var clonesAfter = prototypeExample.GetPrototypeAndClones();
+        }
+        private static void BuilderExecute()
+        {
+
         }
     }
 }
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -18,13 +18,13 @@ namespace Patterns
             //which increase flexibility and reuse of existing code.
             CreationalPattern.Run();
 
-            //Behavioral design patterns are concerned with algorithms and
-            //the assignment of responsibilities between objects.
-            BehavioralPattern.Run();
-
             //Structural design patterns explain how to assemble objects
             //and classes into larger structures, while keeping these structures flexible and efficient.
             StructuralPattern.Run();
+
+            //Behavioral design patterns are concerned with algorithms and
+            //the assignment of responsibilities between objects.
+            BehavioralPattern.Run();
 
             Console.ReadKey();
         }
